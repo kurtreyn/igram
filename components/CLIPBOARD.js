1061a7862e0c48c4dac8f16565a5b56ee69d03b4
KurtReyn <kurtreyn@yahoo.com>
2022-03-25 12:11:48 -0400

    work on posting image

---
+++ components/CLIPBOARD.js
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Button,
+  Image,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  Text,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { firebase, db, storage } from '../../firebase';
+import 'firebase/storage';
+import * as ImagePicker from 'expo-image-picker';
+import BACK_ARROW_ICON from '../../assets/icon-back-arrow.png';
+const backArrowIcon = Image.resolveAssetSource(BACK_ARROW_ICON).uri;
+
+export default function Gallery({ navigation }) {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const [currentLoggedInUser, setCurrentLoggedInUser] = useState(null);
+  const [caption, setCaption] = useState('');
+  // const user = firebase.auth().currentUser;
+
+  // console.log(currentLoggedInUser);
+  // console.log(caption);
+
+  const getUserName = () => {
+    const user = firebase.auth().currentUser;
+    const unsubscribe = db
+      .collection('users')
+      .where('owner_uid', '==', user.uid)
+      .limit(1)
+      .onSnapshot((snapshot) =>
+        snapshot.docs.map((doc) => {
+          setCurrentLoggedInUser({
+            username: doc.data().username,
+            profilePicture: doc.data().profile_picture,
+          });
+        })
+      );
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    getUserName();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      // console.log(`result uri: ${result.uri}`);
+      setImageUrl(result.uri);
+    }
+  };
+
+  // console.log(`imageUrl is: ${imageUrl}`);
+
+  const saveImage = async (uri) => {
+    let filename = uri.substring(uri.lastIndexOf('/') + 1);
+    // console.log(`filename is: ${filename}`);
+    setLoading(true);
+    try {
+      const response = await fetch(uri);
+      const blob = await response.blob();
+      const ref = firebase
+        .storage()
+        .ref()
+        .child('images/' + filename);
+      storage
+        .ref('images/' + filename)
+        .getDownloadURL()
+        .then((url) => {
+          postImage(url + filename, caption);
+        });
+      // console.log(`blob is: ${blob}`);
+      // console.log(`ref is: ${ref}`);
+      ref.put(blob);
+      Alert.alert('Post was successful');
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(error.message);
+    }
+  };
+
+  const postImage = async (img, caption) => {
+    const unsubscribe = db
+      .collection('users')
+      .doc(firebase.auth().currentUser.email)
+      .collection('posts')
+      .add({
+        imageUrl: img,
+        user: currentLoggedInUser.username,
+        profile_picture: currentLoggedInUser.profilePicture,
+        owner_uid: firebase.auth().currentUser.uid,
+        owner_email: firebase.auth().currentUser.email,
+        caption: caption,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        likes_by_users: [],
+        comments: [],
+      })
+      .then(() => navigation.goBack());
+    return unsubscribe;
+  };
+
+  const handlePost = function () {
+    saveImage(imageUrl);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Image
+              source={{
+                uri: backArrowIcon,
+              }}
+              style={{ width: 30, height: 30 }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.bodyContainer}>
+          <Button title="Pick an image from camera roll" onPress={pickImage} />
+          {imageUrl && (
+            <View>
+              <Image
+                source={{ uri: imageUrl }}
+                style={{ width: 200, height: 200 }}
+              />
+              <TextInput
+                style={{ color: 'white', fontSize: 20 }}
+                placeholder="Write a caption..."
+                placeholderTextColor="gray"
+                multiline={true}
+                onChange={(e) => setCaption(e.nativeEvent.text)}
+              />
+              <TouchableOpacity style={styles.button} onPress={handlePost}>
+                <Text style={styles.text}>Post</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+// { flex: 1, alignItems: 'center', justifyContent: 'center' }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  headerContainer: {
+    marginHorizontal: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  bodyContainer: {
+    flex: 1,
+    marginTop: 200,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'lightblue',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
