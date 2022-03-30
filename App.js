403a5a56641a6a1869fcf65470389d328e3d51b3
kurtreyn <kurtreyn@yahoo.com>
2022-03-30 19:30:04 -0400

    test idea

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { firebase, db } from './firebase';
 import AuthNavigation from './AuthNavigation';
 
 export default function App() {
-  const user = firebase.auth().currentUser;
-
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      const uid = user.uid;
-      const displayName = user.displayName;
-      const photoURL = user.photoURL;
-    }
-  });
-
   return (
     <View style={styles.container}>
       <AuthNavigation />
